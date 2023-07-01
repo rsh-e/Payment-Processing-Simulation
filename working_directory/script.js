a12acaf227e1e98d163932d30e804787f57853ff
Hrushikesh Emkay <93153110+rsh-e@users.noreply.github.com>
2023-07-01 22:30:20 +0100

    Delete script.js

--- working_directory/script.js
+++
@@ -1,9 +0,0 @@
-const cardNumberInput = document.getElementById('card-number');
-
-cardNumberInput.addEventListener('input', function (e) {
-    const input = e.target.value.replace(/\D/g, '').substring(0, 16);
-    const spacedInput = input.replace(/(.{4})/g, '$1 ');
-    e.target.value = spacedInput.trim();
-});
-
-
